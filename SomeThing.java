debe9c559f15a42d292326bea82bac1d9278dddc
Leon <angry_yang@163.com>
2018-06-05 10:59:21 +0800

    test

---
+++ SomeThing.java
@@ -0,0 +1,14 @@
+/**
+ * @author leon on 2018/5/31.
+ * @version 1.0
+ */
+public class SomeThing {
+    static int RUN = 1;
+
+    public static void main(String[] args) {
+        int i = 1;
+        if (i == 1 == true) {
+            System.out.println("AA");
+        }
+    }
+}
