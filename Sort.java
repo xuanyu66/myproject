446b815c551b4e334493f859a4649606227ae33c
Leon <angry_yang@163.com>
2018-06-29 16:32:21 +0800

    test

--- Sort.java
+++ Sort.java
@@ -1,7 +1,42 @@
+import java.util.Arrays;
+
 /**
  * @author leon on 2018/6/29.
  * @version 1.0
  */
 public class Sort {
-    
+    static void mergeSort(int[] nums, int start, int end){
+        if(start >= end){
+            return;
+        }
+        int len = end - start, mid =start + (len >> 1);
+        int start1 = start, end1 = mid;
+        int start2 = mid + 1, end2 = end;
+        mergeSort(nums, start1, end1);
+        mergeSort(nums, start2, end2);
+        int[] temp = new int[(end - start) + 1];
+        int i = 0;
+        while(start1 <= end1 && start2 <= end2){
+            temp[i++] = nums[start1] <= nums[start2] ? nums[start1++] : nums[start2++];
+        }
+        while(start1 <= end1){
+            temp[i++] = nums[start1++];
+        }
+        while(start2 <= end2){
+            temp[i++] = nums[start2++];
+        }
+        for (i = start; i <= end ; i++) {
+            nums[i] = temp[i - start];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a =new int[20];
+        for(int i=0;i<a.length;i++) {
+            a[i] = (int) (Math.random() * 100);
+        }
+        System.out.println(Arrays.toString(a));
+        mergeSort(a,0, a.length - 1);
+        System.out.println(Arrays.toString(a));
+    }
 }
